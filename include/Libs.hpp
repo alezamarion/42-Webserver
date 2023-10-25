9f192b6b43b97a00df8ce0aaee12a2926d17d72a
alezamarion <alezamarion@icloud.com>
2023-10-24 21:09:57 -0300

    testing some stuff

---
+++ include/Libs.hpp
@@ -0,0 +1,15 @@
+#ifndef LIBS_HPP
+#define LIBS_HPP
+
+// C++ Libraries
+
+#include <iostream> //Standart input output operation
+#include <csignal>  //Signal handling
+
+
+// C libraries
+
+#include <cstdlib>  //Standard library implementation of C's stdlib.
+
+#endif
+
---
+++ src/main.cpp
@@ -0,0 +1,46 @@
+#include "Libs.hpp"
+
+void stopExecution(int signal)
+{
+    std::cout << '\n';
+    std::cout << "CTRL + C received: freeing up resources" << std::endl;
+    (void)signal;
+    //webserver.stop()
+    exit(0);
+}
+
+void setupSignalHandler()
+{
+    struct sigaction signalHandler;
+    signalHandler.sa_handler = stopExecution;
+    sigemptyset(&signalHandler.sa_mask);       // Clear the signalHandler.sa_mask, meaning no additional signals are blocked during the execution of the signal handler.
+    signalHandler.sa_flags = 0;                // Set sa_flags to 0, indicating no special flags for the signal handler.
+    sigaction(SIGINT, &signalHandler, 0);
+}
+
+int main(int argc, char **argv)
+{
+    if (argc != 2)
+    {
+        if (argc < 2)
+            std::cerr << "Error: missing configuration file" << std::endl;
+        if (argc > 2)
+            std::cerr << "Error: too many arguments" << std::endl;
+        return (1);
+    }
+
+    setupSignalHandler();
+
+    (void)**argv;
+
+    std::cout << "run webserver" << std::endl;
+
+    while (true){}
+
+    //return(webserver.run(argv[1]));
+}
+
+
+
+
+
