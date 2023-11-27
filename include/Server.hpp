221850bc7062edf2da7758bad0d693ef5157a3c6
alezamarion <alezamarion@icloud.com>
2023-11-27 11:12:49 -0300

    create server files

---
+++ include/Server.hpp
@@ -0,0 +1,29 @@
+#ifndef Server_HPP
+#define Server_HPP
+
+#include "Libs.hpp"
+
+class Server
+{
+    public:
+        // Constructors
+        Server(void);
+
+        // Copy constructor
+        Server(Server const &src);
+
+        // Destructor
+        ~Server(void);
+
+        // Copy assignment operator
+        Server &operator=(Server const &rhs);
+
+        //methods:        
+
+
+    private:
+    
+};
+
+#endif
+
